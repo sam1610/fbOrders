ee3b89427c9877c277e1ecb89749fdede1839630
sam1610 <samir.amri@gmail.com>
2025-09-21 20:18:14 +0300

    update Style.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Authenticator } from "@aws-amplify/ui-react";
 import config from '../amplify_outputs.json'; // auto-generated
 import { Amplify } from 'aws-amplify';
 import type { Schema } from '../amplify/data/resource.ts';
 import { generateClient } from 'aws-amplify/api';
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(config);
 
